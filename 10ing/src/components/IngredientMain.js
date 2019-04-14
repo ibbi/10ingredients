f9b641c52400465a09e983353e3f8708b1bbd134
ibbi <hi@ibbi.io>
2019-04-13 17:24:03 -0700

    butt

---
+++ 10ing/src/components/IngredientMain.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './styles/IngredientMain.css';
+
+class IngredientMain extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: true,
+
+        }
+        this.toggleSelected = this.toggleSelected.bind(this);
+    }
+    toggleSelected() {
+        this.setState({
+            selected: !this.state.selected
+        });
+        console.log(this.state)
+    }
+    render() {
+        return (
+            <div className='IngredientMainButton' onClick={this.toggleSelected}>hello</div>
+        )
+    }
+}
+
+export default IngredientMain;
